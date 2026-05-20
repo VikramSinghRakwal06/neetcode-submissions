c1c6d1a62b09c7d5d7e2cd5f662ee29a9b2ed3e5
VikramSinghRakwal06 <vikramrakwal9682@gmail.com>
2026-05-20 17:55:57 +0530

    Add: decode-ways - submission-11

---
+++ Data Structures & Algorithms/decode-ways/submission-11.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.length();
+        if(n==0 || s=="0")return 0;
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]= s[0]=='0'?0:1;
+        for(int i=2;i<=n;i++){
+            if(s[i-1]!='0'){
+                dp[i]+=dp[i-1];
+            }
+            int two = stoi(s.substr(i-2,2));
+            if(two>=10 && two<=26){
+                dp[i]+=dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+};
